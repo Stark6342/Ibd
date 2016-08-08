1e7703cb4e4608045ff931b958911e9351dae623
Betancourt <arael.betancourt@gmail.com>
2016-08-08 01:23:30 -0500

    Cambios Finales

--- FrontEnd/Bienvenida.php
+++ FrontEnd/Bienvenida.php
@@ -25,6 +25,15 @@ require_once "imports2.php";
         <br>
         <h5 class="center">Bienvenida</h5>
         <br><hr><br>
+        <p>¡Bienvenido al sistema óptimo de gestión de la empresa!
+            Disfrute usted de la excelente interfaz de usuario que se le muestra que a la vez es intuitiva y responsiva.
+            Es un Sistema de aplicación encargado de automatizar los procesos que se llevan a cabo en una empresa tomando en cuenta muchos factores que influyen en el comportamiento de esta misma.
+            En este sistema usted podrá gestionar empleados, clientes y pedidos de una manera efectiva.
+            Nosotros quienes desarrollamos este proyecto formamos parte de un grupo de trabajo destinado a la planeación y desarrollo de sistemas de información aprovechando conocimientos de varias herramientas tecnológicas, lenguajes de programación y sistema gestor de base de datos. Para entregar un trabajo que cumpla con todas las expectativas y niveles que el cliente demande.
+            Gracias por utilizar el sistema si tiene problemas por favor contacte al administrador.</p>
+        <br>
+        <h5 class="center">Integrantes</h5>
+        <br><hr><br>
     </div>
 </div>
 
@@ -33,55 +42,44 @@ require_once "imports2.php";
             <div class="card hoverable">
                 <div class="card-image">
                     <img src="Arael.jpg">
-                    <span class="card-title black-text">Betancourt Flores</span>
                 </div>
                 <div class="card-content">
-                    <p>José Arael Betancourt Flores. Alumno de la Universidad Politecnica de Victoria</p>
+                    <p>Betancourt Flores José Arael. Alumno de la Universidad Politecnica de Victoria</p>
                 </div>
             </div>
         </div>
         <div class="col s3 ">
             <div class="card hoverable">
                 <div class="card-image">
-                    <img src="HWc7y.jpg">
-                    <span class="card-title"></span>
+                    <img src="Perlita.jpeg">
                 </div>
                 <div class="card-content">
-                    <p>Alumno de la Universidad Politecnica de Victoria</p>
+                    <p>Gamez Mercado perla Maribel. Alumno de la Universidad Politecnica de Victoria</p>
                 </div>
             </div>
         </div>
-
     </div>
     <div class="row">
         <div class="col s3 m7">
             <div class="card hoverable">
                 <div class="card-image">
-                    <img src="HWc7y.jpg">
-                    <span class="card-title">Betancourt Flores</span>
+                    <img src="Gloria.jpeg">
+                    <span class="card-title"></span>
                 </div>
                 <div class="card-content">
-                    <p>Alumno de la Universidad Politecnica de Victoria</p>
+                    <p>Gloria Vazquez Victor Manuel. Alumno de la Universidad Politecnica de Victoria</p>
                 </div>
             </div>
         </div>
         <div class="col s3 ">
-            <div class="card hoverable purple">
+            <div class="card hoverable ">
                 <div class="card-image">
-                    <img src="HWc7y.jpg">
-                    <span class="card-title purple-text">Betancourt Flores</span>
+                    <img src="Lalo.jpeg">
                 </div>
                 <div class="card-content">
-                    <p>Alumno de la Universidad Politecnica de Victoria</p>
+                    <p>Torres Montalvo José Eduardo.Alumno de la Universidad Politecnica de Victoria</p>
                 </div>
             </div>
         </div>
-
     </div>
-
-
-
-
-
-
 <?php } } ?>
